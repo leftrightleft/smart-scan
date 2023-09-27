487ecd4140808e2b178d0a5b8c4843358fffb446
leftrightleft <leftrightleft@github.com>
2023-09-26 20:28:23 -0700

    logging in init

--- smart-scan/main.py
+++ smart-scan/main.py
@@ -4,14 +4,6 @@ import github
 import sys
 import openai
 
-# Set up logging to file
-logging.basicConfig(filename="bootcamp-setup.log", level=logging.INFO)
-
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.INFO)
-console_formatter = logging.Formatter("%(levelname)s: %(message)s")
-console_handler.setFormatter(console_formatter)
-logging.getLogger().addHandler(console_handler)
 
 open_ai_key = sys.argv[1]
 gh_token = sys.argv[2]
@@ -21,7 +13,7 @@ compare_url = sys.argv[3]
 logging.info("Starting smart-scan")
 logging.info(f"Diff URL: {compare_url}")
 
-print(f"Diff URL: {compare_url}")
+
 def main():
     gh = github.Client(gh_token)
     diff = gh.get_diff(compare_url + ".diff")
