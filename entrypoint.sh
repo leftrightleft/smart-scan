1629b2c32c50fd2d27faf36e56580dd553f5b377
leftrightleft <leftrightleft@github.com>
2023-10-17 15:53:53 -0700

    account for changes in action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh -l
 
 # kick off the command
-echo "Running Smart Scan"
-echo "1: $1"
-echo "2: $2"
-echo "3: $3"
-echo "4: $4"
-echo "5: $5"
-echo "6: $6"
-python /smart_scan/main.py $1 $2 $3 $4 $5
+python /smart_scan/main.py
