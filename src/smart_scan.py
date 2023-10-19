fb1c51eeea2411130bd39ed7de9afe4db4f29f34
leftrightleft <leftrightleft@github.com>
2023-10-19 13:41:54 -0700

    add commenting, read from config

--- src/smart_scan.py
+++ src/smart_scan.py
@@ -7,6 +7,14 @@ import os
 from utils import github, open_ai
 
 
+def get_config(config_file):
+    with open(config_file, "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            raise Exception("Invalid config file. Exiting.")
+
+# Sets the output of the action to the given value
 def set_action_output(value):
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f:
@@ -14,9 +22,21 @@ def set_action_output(value):
 
 
 def main():
+    logging.info("Starting smart scan action")
+
+    logging.info("Retrieving config")
+    try:
+        config_path = os.path.join(os.path.dirname(__file__), "config.yml")
+        config = get_config(config_path)
+    except Exception as e:
+        logging.error(e)
+        set_action_output("yes")
+        sys.exit(1)
+
+    logging.info("Retrieving event context")
     gh_ctx = github.EventContext()
 
-    # Validate inputs and trigger scan if invalid
+    logging.info("Validating inputs")
     try:
         gh_ctx.validate_inputs()
     except Exception as e:
@@ -30,20 +50,29 @@ def main():
         set_action_output("yes")
         sys.exit()
 
-    # Connect to GitHub and retrieve the diff
+    logging.info("Retrieving diff from GitHub")
     gh_client = github.API(gh_ctx.vars["gh_token"])
     try:
         diff = gh_client.get_diff(gh_ctx.diff_url)
     except Exception as e:
+        logging.error(e)
         set_action_output("yes")
         sys.exit(1)
 
-    # Establish OpenAI or Azure client 
+    logging.info("Retrieving decision from OpenAI") 
     if gh_ctx.vars["openai_api_key"]:
-        openai_client = open_ai.OpenAIClient(gh_ctx.vars["openai_api_key"])
+        openai_client = open_ai.OpenAIClient(
+            gh_ctx.vars["openai_api_key"],
+            gh_ctx.vars["model"],
+            config["prompt"],
+            config["temperature"],
+            )
     elif gh_ctx.vars["azure_api_key"]:
         openai_client = open_ai.AzureClient(
             gh_ctx.vars["azure_api_key"],
+            gh_ctx.vars["model"],
+            config["prompt"],
+            config["temperature"],
             gh_ctx.vars["azure_endpoint"],
             gh_ctx.vars["azure_deployment"],
         )
@@ -58,6 +87,17 @@ def main():
 
     logging.info(f"Response decision: {decision['decision']}")
     logging.info(f"Response reason: {decision['reason']}")
+
+
+    # If the decision is "yes", add a comment to the pull request
+    if decision["decision"] == "yes":
+        logging.info("Adding comment to pull request")
+        try:
+            gh_client.add_comment(gh_ctx.comment_url, decision["reason"])
+        except Exception as e:
+            logging.error(e)
+            set_action_output("yes")
+            sys.exit(1)
 
     set_action_output(decision["decision"])
 
