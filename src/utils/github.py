c69ddab542f86ca68285fd2cee8afea0ace1ced7
leftrightleft <leftrightleft@github.com>
2023-10-23 15:21:28 -0700

    fix headers

--- src/utils/github.py
+++ src/utils/github.py
@@ -113,6 +113,11 @@ class API:
             "Accept": "application/vnd.github.v3+json",
             "Content-Type": "application/json",
         }
+        self.diff_headers = {
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/vnd.github.v3.diff",
+            "Content-Type": "application/json",
+        }
 
     def get_diff(self, compare_url):
         """
@@ -124,7 +129,7 @@ class API:
         Returns:
             str: The diff for the specified commit range.
         """
-        response = requests.get(compare_url, headers=self.headers)
+        response = requests.get(compare_url, headers=self.diff_headers)
         if response.status_code == 200:
             logging.info("Successfully retrieved diff")
             return response.text
